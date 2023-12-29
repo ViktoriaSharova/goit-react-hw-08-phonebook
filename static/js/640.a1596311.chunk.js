8b905a3c1927de3a4b18a79237f70a2a684f1426
ViktoriaSharova <131592560+ViktoriaSharova@users.noreply.github.com>
2023-12-29 13:37:27 +0000

    Deploying to gh-pages from @ ViktoriaSharova/goit-react-hw-08-phonebook@24fe8be16693376ca976a5cf0a440c87dd125c08 🚀

---
+++ static/js/640.a1596311.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[640],{7640:function(e,r,t){t.r(r),t.d(r,{default:function(){return a}});var n=t(1087),o=t(184);function a(){return(0,o.jsxs)("div",{children:["Sorry! Please go ",(0,o.jsx)(n.rU,{to:"/",children:"Home"})," page"]})}}}]);
+//# sourceMappingURL=640.a1596311.chunk.js.map
